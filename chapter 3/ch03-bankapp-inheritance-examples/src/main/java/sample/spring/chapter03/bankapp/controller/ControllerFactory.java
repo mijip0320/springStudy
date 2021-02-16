0e363350a919a91c120bc37f314f705386d1aa5a
mijip0320 <mijip0320@gmail.com>
2021-02-16 09:48:10 +0900

    update spring study

--- chapter 3/ch03-bankapp-inheritance-examples/src/main/java/sample/spring/chapter03/bankapp/controller/ControllerFactory.java
+++ chapter 3/ch03-bankapp-inheritance-examples/src/main/java/sample/spring/chapter03/bankapp/controller/ControllerFactory.java
@@ -1,6 +1,8 @@
 package sample.spring.chapter03.bankapp.controller;
 
 public class ControllerFactory {
+
+	//자신에게 전달된 controllerName 인수값에 따라 FixedDepositControllerImpl이나 PersonalBankingControllerImpl 클래스를 생성
 	public Object getController(String controllerName) {
 		Object controller = null;
 		if ("fixedDepositController".equalsIgnoreCase(controllerName)) {
